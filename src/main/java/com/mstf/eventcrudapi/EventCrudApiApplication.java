ff63ad57c3208a0c3f4106e424dce3674168ec57
Mostafa Sissi <mostafasissi@gmail.com>
2023-11-05 22:12:40 +0100

    first commit

---
+++ src/main/java/com/mstf/eventcrudapi/EventCrudApiApplication.java
@@ -0,0 +1,16 @@
+package com.mstf.eventcrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCrudApiApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EventCrudApiApplication.class, args);
+        System.out.println("Hello word");
+    }
+
+}
---
+++ src/test/java/com/mstf/eventcrudapi/EventCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mstf.eventcrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCrudApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
